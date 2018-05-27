eef712c39919c05776b2ce6bd932eb410aa1a582
dwicao <lutfiandwica@gmail.com>
2018-05-27 08:34:05 +0700

    adds functionality for replace chain

--- blockchain.js
+++ blockchain.js
@@ -1,20 +1,7 @@
 const Block = require('./block');
 
 class Blockchain {
-  constructor() {
-    this.chain = [Block.genesis()];
-  }
-
-  addBlock(data) {
-    const lastBlock = this.chain[this.chain.length - 1];
-    const block = Block.mineBlock(lastBlock, data);
-
-    this.chain.push(block);
-
-    return block;
-  }
-
-  isValidChain(chain) {
+  static isValidChain(chain) {
     if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) {
       return false;
     }
@@ -32,6 +19,34 @@ class Blockchain {
 
       return true;
     }
+  }
+
+  constructor() {
+    this.chain = [Block.genesis()];
+  }
+
+  addBlock(data) {
+    const lastBlock = this.chain[this.chain.length - 1];
+    const block = Block.mineBlock(lastBlock, data);
+
+    this.chain.push(block);
+
+    return block;
+  }
+
+  replaceChain(newChain) {
+    if (newChain.length <= this.chain.length) {
+      console.log('Received chain is not longer than the current chain.');
+      return;
+    }
+
+    if (!Blockchain.isValidChain(newChain)) {
+      console.log('The received chian in not valid.');
+      return;
+    }
+
+    console.log('Replacing blockchain with new chain.');
+    this.chain = newChain;
   }
 }
 
--- blockchain.test.js
+++ blockchain.test.js
@@ -24,19 +24,33 @@ describe('Blockchain', () => {
   it('validates a valid chain', () => {
     bc2.addBlock('foo');
 
-    expect(bc.isValidChain(bc2.chain)).toBe(true);
+    expect(Blockchain.isValidChain(bc2.chain)).toBe(true);
   });
 
   it('invalidates a chain with a corrupt genesis block', () => {
     bc2.chain[0].data = 'Bad data';
 
-    expect(bc.isValidChain(bc2.chain)).toBe(false);
+    expect(Blockchain.isValidChain(bc2.chain)).toBe(false);
   });
 
   it('invalidates a corrupt chain', () => {
     bc2.addBlock('foo');
     bc2.chain[1].data = 'Not foo';
 
-    expect(bc.isValidChain(bc2.chain)).toBe(false);
+    expect(Blockchain.isValidChain(bc2.chain)).toBe(false);
+  });
+
+  it('replaces the chain with a valid chain', () => {
+    bc2.addBlock('goo');
+    bc.replaceChain(bc2.chain);
+
+    expect(bc.chain).toEqual(bc2.chain);
+  });
+
+  it('does not replace the chain with one of less than or equal to length', () => {
+    bc.addBlock('foo');
+    bc.replaceChain(bc2.chain);
+
+    expect(bc.chain).not.toEqual(bc2.chain);
   });
 });
