e74f383b16b450d9bed2cc748408efa0b0223668
dwicao <lutfiandwica@gmail.com>
2018-05-27 12:21:03 +0700

    connect blockchain to peers

--- app/p2p-server.js
+++ app/p2p-server.js
@@ -13,7 +13,18 @@ class P2pServer {
     const server = new Websocket.Server({port: P2P_PORT});
 
     server.on('connection', socket => this.connectSocket(socket));
+
+    this.connectToPeers();
+
     console.log(`Listening for peer-to-peer connections on: ${P2P_PORT}`);
+  }
+
+  connectToPeers() {
+    peers.forEach(peer => {
+      const socket = new Websocket(peer);
+
+      socket.on('open', () => this.connectSocket(socket));
+    });
   }
 
   connectSocket(socket) {
@@ -21,3 +32,5 @@ class P2pServer {
     console.log('Socket connected');
   }
 }
+
+module.exports = P2pServer;
