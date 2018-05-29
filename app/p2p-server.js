c8c0af35a6e0ee1ae1e19793bb564256fefc89c5
dwicao <lutfiandwica@gmail.com>
2018-05-29 09:03:08 +0700

    handle messages from peers

--- app/p2p-server.js
+++ app/p2p-server.js
@@ -30,6 +30,17 @@ class P2pServer {
   connectSocket(socket) {
     this.sockets.push(socket);
     console.log('Socket connected');
+
+    this.messageHandler(socket);
+
+    socket.send(JSON.stringify(this.blockchain.chain));
+  }
+
+  messageHandler(socket) {
+    socket.on('message', message => {
+      const data = JSON.parse(message);
+      console.log('data', data);
+    });
   }
 }
 
