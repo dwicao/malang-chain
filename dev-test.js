f7cd8b17379081e3b80e4f838dd5d3446e66ee3d
dwicao <lutfiandwica@gmail.com>
2018-06-01 09:52:25 +0700

    init wallet

--- dev-test.js
+++ dev-test.js
@@ -1,7 +1,4 @@
-const Blockchain = require('./blockchain');
+const Wallet = require('./wallet');
+const wallet = new Wallet();
 
-const bc = new Blockchain();
-
-for (let i = 0; i < 10; i++) {
-  console.log(bc.addBlock(`foo ${i}`).toString());
-}
+console.log(wallet.toString());
---
+++ wallet/index.js
@@ -0,0 +1,19 @@
+const {INITIAL_BALANCE} = require('../config');
+const ChainUtils = require('../utils/chain');
+
+class Wallet {
+  constructor() {
+    this.balance = INITIAL_BALANCE;
+    this.keyPair = ChainUtils.genKeyPair();
+    this.publicKey = this.keyPair.getPublic().encode('hex');
+  }
+
+  toString() {
+    return `Wallet -
+      Public Key  : ${this.publicKey.toString()}
+      Balance     : ${this.balance}
+    `;
+  }
+}
+
+module.exports = Wallet;
