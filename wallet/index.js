42d53307b403aa21e92fc9437b7def16e7b4d703
dwicao <lutfiandwica@gmail.com>
2018-06-20 09:57:34 +0700

    create transaction with wallet

--- wallet/index.js
+++ wallet/index.js
@@ -1,5 +1,6 @@
 const {INITIAL_BALANCE} = require('../config');
 const ChainUtils = require('../utils/chain');
+const Transaction = require('./transaction');
 
 class Wallet {
   constructor() {
@@ -17,6 +18,24 @@ class Wallet {
 
   sign(dataHash) {
     return this.keyPair.sign(dataHash);
+  }
+
+  createTransaction(recipient, amount, transactionPool) {
+    if (amount > this.balance) {
+      console.log(`Amount: ${amount} exceeds current balance: ${this.balance}`);
+      return;
+    }
+
+    let transaction = transactionPool.existingTransaction(this.publicKey);
+
+    if (transaction) {
+      transaction.update(this, recipient, amount);
+    } else {
+      transaction = Transaction.newTransaction(this, recipient, amount);
+      transactionPool.updateOrAddTransaction(transaction);
+    }
+
+    return transaction;
   }
 }
 
