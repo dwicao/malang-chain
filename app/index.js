6a0d876b8feee196f6766ee54dfd37daaf73ab7b
dwicao <lutfiandwica@gmail.com>
2018-06-20 13:40:30 +0700

    handle transaction in p2p server

--- app/index.js
+++ app/index.js
@@ -11,7 +11,7 @@ const app = express();
 const bc = new Blockchain();
 const wallet = new Wallet();
 const tp = new TransactionPool();
-const p2pServer = new P2pServer(bc);
+const p2pServer = new P2pServer(bc, tp);
 
 app.use(bodyParser.json());
 
@@ -36,6 +36,7 @@ app.post('/transact', (req, res) => {
   const {recipient, amount} = req.body;
   const transaction = wallet.createTransaction(recipient, amount, tp);
 
+  p2pServer.broadcastTransaction(transaction);
   res.redirect('/transactions');
 });
 
