25396738f78fb40c922d8ba7991dc5207e7a8399
dwicao <lutfiandwica@gmail.com>
2018-06-20 13:06:58 +0700

    get transactions

--- app/index.js
+++ app/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
+const Wallet = require('../wallet');
+const TransactionPool = require('../wallet/transaction-pool');
 const P2pServer = require('./p2p-server');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
+const wallet = new Wallet();
+const tp = new TransactionPool();
 const p2pServer = new P2pServer(bc);
 
 app.use(bodyParser.json());
@@ -22,6 +26,10 @@ app.post('/mine', (req, res) => {
   p2pServer.syncChains();
 
   res.redirect('/blocks');
+});
+
+app.get('/transactions', (req, res) => {
+  res.json(tp.transactions);
 });
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
