40f64f32a43114863c32619187fc2343bb80c3c8
dwicao <lutfiandwica@gmail.com>
2018-06-21 07:03:42 +0700

    add mine transactions endpoint

--- app/index.js
+++ app/index.js
@@ -4,6 +4,7 @@ const Blockchain = require('../blockchain');
 const Wallet = require('../wallet');
 const TransactionPool = require('../wallet/transaction-pool');
 const P2pServer = require('./p2p-server');
+const Miner = require('./miner');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
@@ -12,6 +13,7 @@ const bc = new Blockchain();
 const wallet = new Wallet();
 const tp = new TransactionPool();
 const p2pServer = new P2pServer(bc, tp);
+const miner = new Miner(bc, tp, wallet, p2pServer);
 
 app.use(bodyParser.json());
 
@@ -38,6 +40,13 @@ app.post('/transact', (req, res) => {
 
   p2pServer.broadcastTransaction(transaction);
   res.redirect('/transactions');
+});
+
+app.get('/mine-transactions', (req, res) => {
+  const block = miner.mine();
+
+  console.log(`New block added: ${block.toString()}`);
+  res.redirect('/blocks');
 });
 
 app.get('/public-key', (req, res) => {
